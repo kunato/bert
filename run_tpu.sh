55c70740272c304728da7756142764916f47eb8f
kunato <kunato36@gmail.com>
2019-01-05 16:48:55 +0700

    add pipenv and cpu setup script.

---
+++ run_tpu.sh
@@ -0,0 +1,18 @@
+export DATA_DIR=/home/kunato/bert_in
+export BERT_BASE_DIR=/home/kunato/bert_out
+
+python run_pretraining.py \
+  --input_file=$DATA_DIR/tf_examples.tfrecord \
+  --output_dir=$BERT_BASE_DIR \
+  --use_tpu=True \
+  --tpu_name=demo-tpu \
+  --do_train=True \
+  --do_eval=True \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --train_batch_size=32 \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --num_train_steps=1000000 \
+  --num_warmup_steps=100000 \
+  --learning_rate=1e-4 \
+  --save_checkpoints_steps=200000
---
+++ setup_tpu.sh
@@ -0,0 +1,3 @@
+gcloud config set project "ink-personal"
+gcloud config set compute/zone us-central1-b
+gcloud compute tpus list
